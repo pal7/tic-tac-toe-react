cdd18e17bf32e1bec8763ba5ef23f6ad400dd25c
Ashwin <ashwinpalaniappanr@gmail.com>
2025-01-26 00:02:27 -0500

    gameBoard state

--- src/components/GameBoard.jsx
+++ src/components/GameBoard.jsx
@@ -1,20 +1,36 @@
-import React from "react";
+import React, { useState } from "react";
 
 const initialGameBoard = [
-  [null, "X", "O"],
+  [null, null, null],
   [null, null, null],
   [null, null, null],
 ];
 
 export default function GameBoard() {
+  const [gameBoard, setGameBoard] = useState(initialGameBoard);
+
+  function handleSquareClick(rowIndex, colIndex) {
+    // console.log(rowIndex, colIndex);
+    setGameBoard((prevGameBoard) => {
+      // deep copy of object/array to make the state object(array here) immutable
+      const updatedBoard = [...prevGameBoard].map((innerArray) => [
+        ...innerArray,
+      ]);
+      updatedBoard[rowIndex][colIndex] = "X";
+      return updatedBoard;
+    });
+  }
+
   return (
     <ol id="game-board">
-      {initialGameBoard.map((row, rowIndex) => (
+      {gameBoard.map((row, rowIndex) => (
         <li key={rowIndex}>
           <ol>
             {row.map((playerSymbol, colIndex) => (
               <li key={colIndex}>
-                <button>{playerSymbol}</button>
+                <button onClick={() => handleSquareClick(rowIndex, colIndex)}>
+                  {playerSymbol}
+                </button>
               </li>
             ))}
           </ol>
