cff46b1e6e874e2dd6c35a0f6cb4c94c021d1a4c
Ashwin <ashwinpalaniappanr@gmail.com>
2025-01-27 03:41:37 -0500

    list key bug fix

--- src/components/Log.jsx
+++ src/components/Log.jsx
@@ -2,7 +2,7 @@ export default function Log({ turns }) {
   return (
     <ol id="log">
       {turns.map((turn) => (
-        <li key={`${turn.player.row}${turn.player.col}`}>
+        <li key={`${turn.square.row}${turn.square.col}`}>
           {turn.player} selected {turn.square.row},{turn.square.col}
         </li>
       ))}
